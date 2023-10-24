3e7e7b703e930bfd0fe17a9242c9a1e1c36ffdc7
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2023-10-24 08:59:08 +0300

    Update debugging.py

--- emobilis/debugging/debugging.py
+++ emobilis/debugging/debugging.py
@@ -1,1 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#using print
+
+#print("hello")
+
+
+# def add(nums):
+    
+#     for num in nums:
+#         print("debugging!")
+#         if num<1:
+#             print("debugging 1")
+#             print("oops!")           
+                        
+#     return num  
+
+# add([1,2,3,4])
+
+def add(nums):
+    
+    for num in nums:
+        #get to know what's going on in the 
+        import pdb
+        pdb.set_trace()
+        if num<1:
+            print("debugging 1")
+            print("oops!")           
+                        
+    return num  
+
+add([1,2,3,4])
+
+
+# In[ ]:
+
+
+
+
 
